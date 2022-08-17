12ff59c74febb037557f32fe70381e8acb43ebef
daryatalantseva <d.talantseva@gmail.com>
2022-08-17 18:05:13 +0300

    Задачи семинар 5

---
+++ sem5_34/Program.cs
@@ -0,0 +1,21 @@
+﻿
+/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+Console.WriteLine("Введите количество элементов в массиве");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int rez = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i] % 2 == 0)
+    {
+        rez++;
+    }
+    Console.Write(array[i] + " ");
+
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных элементов равно " + rez);
+
---
+++ sem5_38/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+Console.WriteLine("Введите количество элементов в массиве");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int rez = 0;
+int max = 1;
+int min = 100;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+    Console.Write(array[i] + " ");
+
+}
+rez = max - min;
+Console.WriteLine();
+Console.WriteLine(" Минимальное значение равно " + min + " Максимальное значение равно " + max);
+Console.WriteLine("Разница между максимальным и минимальным равна " + rez);
