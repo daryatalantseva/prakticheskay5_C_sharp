95a98bfbb125cd749190160f6976c281924c7334
daryatalantseva <d.talantseva@gmail.com>
2022-08-18 12:39:48 +0300

    Задачи семинар 5

--- sem5_38/Program.cs
+++ sem5_38/Program.cs
@@ -1,14 +1,13 @@
 ﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76 */
-Console.WriteLine("Введите количество элементов в массиве");
-int N = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[N];
-int rez = 0;
-int max = 1;
-int min = 100;
+double[] array = new double[10] { 1.05, 2.5, 2.3, 8.5, 6.5, 7.2, 4.3, 7.6, 4.2, 5.1 };
+
+double rez = 0;
+double max = array[0];
+double min = array[0];
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(1,100);
+  
     if (array[i] > max)
     {
         max = array[i];
